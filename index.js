ddf3fdf0690e87909b54b4fa651f46e12ac7ff80
Mohsen Azimi <msn.azi@gmail.com>
2014-05-23 15:46:59 -0700

    add plyfills

--- index.js
+++ index.js
@@ -1,11 +1,77 @@
 (function(){
-  _.deepPick = function(object, json){
-    object = _.pick(object, Object.keys(json));
-    Object.keys(json).forEach(function(key){
-      if(json[key] instanceof Object){
-        object[key] = _.deepPick(object[key], json[key]);
+
+  // Array.prototype.forEach Polyfill from MDN
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function(fun /*, thisArg */) {
+      "use strict";
+
+      if (this === void 0 || this === null)
+        throw new TypeError();
+
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (typeof fun !== "function")
+        throw new TypeError();
+
+      var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+      for (var i = 0; i < len; i++){
+        if (i in t)
+          fun.call(thisArg, t[i], i, t);
+      }
+    };
+  }
+
+  // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  if (!Object.keys) {
+    Object.keys = (function () {
+      'use strict';
+      var hasOwnProperty = Object.prototype.hasOwnProperty,
+      hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+      dontEnums = [
+        'toString',
+        'toLocaleString',
+        'valueOf',
+        'hasOwnProperty',
+        'isPrototypeOf',
+        'propertyIsEnumerable',
+        'constructor'
+      ],
+      dontEnumsLength = dontEnums.length;
+
+      return function (obj) {
+        if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+          throw new TypeError('Object.keys called on non-object');
+        }
+
+        var result = [], prop, i;
+
+        for (prop in obj) {
+          if (hasOwnProperty.call(obj, prop)) {
+            result.push(prop);
+          }
+        }
+
+        if (hasDontEnumBug) {
+          for (i = 0; i < dontEnumsLength; i++) {
+            if (hasOwnProperty.call(obj, dontEnums[i])) {
+              result.push(dontEnums[i]);
+            }
+          }
+        }
+        return result;
       };
-    });
-    return object;
-  };
+    }());
+  }
+
+
+
+_.deepPick = function(object, json){
+  object = _.pick(object, Object.keys(json));
+  Object.keys(json).forEach(function(key){
+    if(json[key] instanceof Object){
+      object[key] = _.deepPick(object[key], json[key]);
+    };
+  });
+  return object;
+};
 })();
