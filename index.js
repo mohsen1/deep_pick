be6b3e61caa49b99c9925358216a79320cb79717
Mohsen Azimi <msn.azi@gmail.com>
2014-05-23 15:32:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+_.deepPick = function(object, json){
+  object = _.pick(object, Object.keys(json));
+  Object.keys(json).forEach(function(key){
+    if(json[key] instanceof Object){
+      object[key] = _.deepPick(object[key], json[key]);
+    };
+  });
+  return object;
+};
