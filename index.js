5d99c7814cc7b6979661f04314c910767eeca475
Mohsen Azimi <msn.azi@gmail.com>
2014-05-23 15:33:16 -0700

    Put it in IIFE

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-_.deepPick = function(object, json){
-  object = _.pick(object, Object.keys(json));
-  Object.keys(json).forEach(function(key){
-    if(json[key] instanceof Object){
-      object[key] = _.deepPick(object[key], json[key]);
-    };
-  });
-  return object;
-};
+(function(){
+  _.deepPick = function(object, json){
+    object = _.pick(object, Object.keys(json));
+    Object.keys(json).forEach(function(key){
+      if(json[key] instanceof Object){
+        object[key] = _.deepPick(object[key], json[key]);
+      };
+    });
+    return object;
+  };
+})();
