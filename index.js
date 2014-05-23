b4b2d080947b8ce1c8d86f393f6b06fd55d57d68
Mohsen Azimi <msn.azi@gmail.com>
2014-05-23 15:47:59 -0700

    Indentation

--- index.js
+++ index.js
@@ -65,13 +65,13 @@
 
 
 
-_.deepPick = function(object, json){
-  object = _.pick(object, Object.keys(json));
-  Object.keys(json).forEach(function(key){
-    if(json[key] instanceof Object){
-      object[key] = _.deepPick(object[key], json[key]);
-    };
-  });
-  return object;
-};
+  _.deepPick = function(object, json){
+    object = _.pick(object, Object.keys(json));
+    Object.keys(json).forEach(function(key){
+      if(json[key] instanceof Object){
+        object[key] = _.deepPick(object[key], json[key]);
+      };
+    });
+    return object;
+  };
 })();
